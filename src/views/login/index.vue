758c3634683b425ae46c990b5e048d5602225e55
Richie <yuqi.gong.home@gmail.com>
2018-08-16 10:41:02 +0800

    login validate

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,22 +5,29 @@
         <svg-icon icon-class="logo" :style="{ transform: 'scale(2.9)', marginRight: '14px', marginBottom: '3px' }"/>
         <span>上证信息运维自动化平台</span>
       </div>
-      <div class="tips">
-        美好的一天，从现在开始(这是一段好几个字的副标题)
+      <div class="tips" style="text-align:center;">
+        美好的一天，从现在开始
       </div>
       <el-form-item prop="username">
-        <span class="svg-container svg-container_login">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="用户名" />
+        <el-input
+          placeholder="用户名"
+          v-model="loginForm.username"
+          autoComplete="on" 
+          name="username" >
+          <svg-icon slot="prefix" icon-class="user"></svg-icon>
+        </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password"></svg-icon>
-        </span>
-        <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-          placeholder="密码"></el-input>
-          <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>
+        <el-input
+          placeholder="密码"
+          :type="pwdType"
+          v-model="loginForm.password"
+          autoComplete="on" 
+          @keyup.enter.native="handleLogin"
+          name="username" >
+          <svg-icon slot="prefix" icon-class="password"></svg-icon>
+        </el-input>
+        <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>
       </el-form-item>
       <div style="text-align:right;margin-bottom: 15px;" >
         <el-checkbox v-model="checked">自动登录</el-checkbox>
@@ -53,34 +60,35 @@
 </template>
 
 <script>
-import { isvalidUsername } from '@/utils/validate'
+// import { isvalidUsername } from '@/utils/validate'
 
 export default {
   name: 'login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!isvalidUsername(value)) {
-        callback(new Error('请输入正确的用户名'))
-      } else {
-        callback()
-      }
-    }
-    const validatePass = (rule, value, callback) => {
-      if (value.length < 5) {
-        callback(new Error('密码不能小于5位'))
-      } else {
-        callback()
-      }
-    }
+    // const validateUsername = (rule, value, callback) => {
+    //   if (!isvalidUsername(value)) {
+    //     callback(new Error('请输入正确的用户名'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
+    // const validatePass = (rule, value, callback) => {
+    //   if (value.length < 5) {
+    //     callback(new Error('密码不能小于5位'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
     return {
       loginForm: {
         username: 'admin',
         password: 'admin'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePass }]
+        username: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
+        password: [{ required: true, message: '密码不能为空', trigger: 'blur' }]
       },
+      checked: false,
       loading: false,
       pwdType: 'password'
     }
@@ -119,16 +127,19 @@ $light_gray:#000;
 
 /* reset element-ui css */
 .login-container {
+  .el-input__prefix{
+    left: 15px;
+    top: 4px;
+  }
   .el-input {
     display: inline-block;
     height: 47px;
-    width: 85%;
+    width: 100%;
     input {
       background: transparent;
-      border: 0px;
       -webkit-appearance: none;
       border-radius: 0px;
-      padding: 12px 5px 12px 15px;
+      padding: 12px 5px 12px 40px;
       color: $light_gray;
       height: 47px;
       &:-webkit-autofill {
@@ -138,7 +149,6 @@ $light_gray:#000;
     }
   }
   .el-form-item {
-    border: 1px solid #D9D9D9;
     background: rgb(255, 255, 255);
     border-radius: 5px;
     color: #454545;
@@ -190,7 +200,7 @@ $light_gray:#242424;
   .show-pwd {
     position: absolute;
     right: 10px;
-    top: 7px;
+    top: 5px;
     font-size: 16px;
     color: $dark_gray;
     cursor: pointer;
