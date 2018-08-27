f5013da3a16bc27fe6e0daeef0c55c2279aa6518
wanglin <wanglin@zhongan.io>
2018-08-27 14:17:42 +0800

    全局添加对业务组更新后的刷新

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="app-main">
     <transition name="fade" mode="out-in">
-      <!-- <router-view :key="key"></router-view> -->
-      <router-view></router-view>
+      <router-view :key="activeKey"></router-view>
+      <!-- <router-view></router-view> -->
     </transition>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'AppMain',
+  data() {
+    return {
+      activeKey: +new Date()
+    }
+  },
   computed: {
+    ...mapGetters([
+      'repository'
+    ])
     // key() {
     //   return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
     // }
+  },
+  watch: {
+    repository(val, oldVal) {
+      if (val !== oldVal) {
+        this.activeKey = +new Date()
+      }
+    }
   }
 }
 </script>
